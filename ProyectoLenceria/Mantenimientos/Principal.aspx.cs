c194191ed4c71d9c87a0f0b9e97a110330986a9a
Lalo Herrera <tensazangetsu616@outlook.es>
2021-11-28 20:56:56 -0500

    Metodo saludo creado

--- ProyectoLenceria/Mantenimientos/Principal.aspx.cs
+++ ProyectoLenceria/Mantenimientos/Principal.aspx.cs
@@ -11,7 +11,26 @@ namespace ProyectoLenceria.Mantenimientos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            CargaHora();
+        }
 
+        private void CargaHora()
+        {
+            string hora = DateTime.Now.ToString("HH");
+            int time = Convert.ToInt32(hora);
+
+            if (time>7 && time<12)
+            {
+                lblSaludo.Text = "Buen Día";
+            }
+            else if (time>=12 && time<18)
+            {
+                lblSaludo.Text = "Buenas Tardes";
+            }
+            else
+            {
+                lblSaludo.Text = "Buenas Noches";
+            }
         }
     }
 }
